acfe6c2f532c5df0493433ba81e01a42aef25768
Igorcar <grabovac.igor@gmail.com>
2020-08-11 16:13:38 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,88 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hero';
+  
+  month = 0;
+  day = 0;
+  carName = "";
+  color=false;
+  colorName="";
+  
+  monthValue = (value) => {
+    let val = parseInt(value);
+    this.month = val;
+  }
+
+  dayValue = (value) => {
+    let val = parseInt(value);
+    this.day = val;
+  }
+  haveColor(){
+    this.color = !this.color;
+  }
+  colorNameFunc=(val) =>{
+    this.colorName = val;
+  }
+
+  generateName(){
+    
+
+    this.carName = "";
+    if(this.color){
+      this.carName += `The ${this.colorName}`;
+      this.carName += this.dayNames[this.day-1];
+    }
+    else{this.carName +=this.monthNames[this.month-1];
+      this.carName += this.dayNames[this.day-1];}
+    
+  }
+  monthNames = [
+  "The Agressive",
+  "The Mighty",
+  "The Powerful",
+  "The Mad",
+  "The Furious",
+  "The Destroying" ,
+  "The Agressive",
+  "The Mighty",
+  "The Powerful",
+  "The Mad",
+  "The Furious",
+  "The Destroying" ,
+  ]
+
+  dayNames = [
+    " Wolf",
+    " Beer",
+    " Tiger",
+    " Lion",
+    " Shark",
+    " Titan",
+    " Giant",
+    " Machine",
+    " Monster",
+    " Godzilla",
+    " Shakal",
+    " Cobra",
+    " Snake",
+    " T-Rex",
+    " Cunami",
+    " Tornado",
+    " Conqueror",
+    " Imperator",
+    " Rocket",
+    " Bomb",
+    " Tiger",
+    " Lion",
+    " Shark",
+    " Titan",
+    " Giant",
+    " Machine",
+    " Monster",
+    " Godzilla",
+    " Shakal",
+    " Cobra",
+    " Snake"
+  ]
+
 }
